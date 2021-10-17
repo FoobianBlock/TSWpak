803f990cfe0857de6e51588af50fd062d328d1c2
FoobianBlock <89279039+FoobianBlock@users.noreply.github.com>
2021-10-17 15:38:54 +0200

    Add project files.

---
+++ TSWpak/Program.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSWpak
+{
+    class Program
+    {
+        static bool StartedFromGui =
+           !Console.IsOutputRedirected
+        && !Console.IsInputRedirected
+        && !Console.IsErrorRedirected
+        && Environment.UserInteractive
+        && Environment.CurrentDirectory == System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location)
+        && Console.CursorTop == 0 && Console.CursorLeft == 0
+        && Console.Title == Environment.GetCommandLineArgs()[0]
+        && Environment.GetCommandLineArgs()[0] == System.Reflection.Assembly.GetEntryAssembly().Location;
+
+        static void Main(string[] args)
+        {
+            string usage = "Packaging Syntax:\n[Mod content path] [Pak file name (optional)] [UnrealPak.exe path (optional)]\n\n" +
+                "Commands:\n" +
+                "RESET  Deletes saved UnrealPak.exe path\n" +
+                "HELP   Opens this very list\n" +
+                "INFO   Programm info\n";
+            string info = System.Reflection.Assembly.GetEntryAssembly().GetName().Name + " " 
+                + System.Reflection.Assembly.GetEntryAssembly().GetName().Version.ToString() + " made by Foobian";
+
+            string savedUnrealPakPath = ReadSetting("UnrealPakPath");
+
+            string unrealPakPath = null;
+            string contentPath = null;
+            string pakName = null;
+
+            if (args.Length > 0)
+            {
+                if (args[0].ToLower() == "help" | args[0] == "?")
+                {
+                    Console.WriteLine(usage);
+                    Exit();
+                }
+                else if (args[0].ToLower() == "info" | args[0].ToLower() == "ver" | args[0].ToLower() == "version")
+                {
+                    WriteColoredLine(info, ConsoleColor.Blue);
+                    Exit();
+                }
+                else if (args[0].ToLower() == "reset")
+                {
+                    // Resets config file
+                    UpdateAppSetting("UnrealPakPath", "");
+                    Exit();
+                }
+                else if (args[0].ToLower() == "readout")
+                {
+                    // Readout of all the keys from the config file
+                    NameValueCollection sAll;
+                    sAll = ConfigurationManager.AppSettings;
+
+                    foreach (string s in sAll.AllKeys)
+                        Console.WriteLine("Key: " + s + " Value: " + sAll.Get(s));
+                    Exit();
+                }
+                else
+                {
+                    contentPath = args[0];
+
+                    if (args.Length == 2)
+                        pakName = args[1];
+
+                    if (args.Length == 3)
+                    {
+                        unrealPakPath = args[2];
+                        WriteColoredLine("If the inputed UnrealPak path is confirmed working it will be stores as the defaut path", ConsoleColor.DarkGray);
+                    }
+
+                    if(String.IsNullOrWhiteSpace(savedUnrealPakPath) && String.IsNullOrWhiteSpace(unrealPakPath))
+                    {
+                        WriteColoredLine("It looks like there is no default UnrealPak.exe path specified, please enter one bellow! If confirmed working, " +
+                            "it will be used as the default path if no path is given. You can use the command \"reset\" to reset the path if anything goes wrong.", ConsoleColor.Blue);
+
+                        Console.Write("UnrealPak.exe file path: ");
+                        unrealPakPath = Console.ReadLine();
+                    }
+                    else
+                    {
+                        unrealPakPath = savedUnrealPakPath;
+                        WriteColoredLine("Using stored UnrealPak.exe path", ConsoleColor.DarkGray);
+                    }
+
+                    if (args.Length > 3)
+                    {
+                        WriteColoredLine("Invalid number of arguments", ConsoleColor.Red);
+                        Console.WriteLine(usage);
+                        Exit(0);
+                    }
+                }
+            }
+            else
+            {
+                if(StartedFromGui)
+                    Console.WriteLine("Please run TSWpak.exe from a cmd or PowerShell window with following commands:");
+
+                Console.WriteLine(usage);
+                Exit(0);
+
+                /*
+                Console.Write("Mod content file path: ");
+                contentPath = Console.ReadLine();
+                Console.Write("pak name/file path (optional): ");
+                pakName = Console.ReadLine();
+                Console.Write("UnrealPak.exe file path: ");
+                unrealPakPath = Console.ReadLine();
+                */
+            }
+
+            #region Idiot Protection
+            if(!unrealPakPath.Contains(".exe"))
+            {
+                WriteColoredLine("Your selected path to UnrealPak.exe (" + unrealPakPath + ") does not end in .exe! " +
+                    "Please enter the path to UnrealPak.exe including the exe-file.", ConsoleColor.Red);
+                Exit(2);
+            }
+
+            TestDir(contentPath);
+
+            if (contentPath.EndsWith("TS2Prototype") | contentPath.EndsWith("TS2Prototype\\"))
+            {
+                WriteColoredLine("Please select the folder above your \"TS2Prototype\" Folder, otherwise Train Sim World will not see the mod. " +
+                    "This folder should be empty", ConsoleColor.Red);
+
+                ContinueQuestion:
+                WriteColoredLine("Do you still want to continue? Y/N", ConsoleColor.Yellow);
+
+                char questionAnswer = Console.ReadKey().KeyChar;
+                switch (questionAnswer.ToString().ToLower())
+                {
+                    case "y":
+                        Console.Write("\n");
+                        break;
+                    case "n":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        goto ContinueQuestion;
+                }
+            }
+
+            // Check if "TS2Prototype" sub-folder exists
+            DirectoryInfo contentDirInfo = new DirectoryInfo(contentPath);
+            DirectoryInfo[] contentDirSubs = contentDirInfo.GetDirectories();
+            bool ts2DirExists = false;
+            foreach (DirectoryInfo dir in contentDirSubs)
+            {
+                if(dir.Name == "TS2Prototype")
+                {
+                    ts2DirExists = true;
+                }
+            }
+
+            FileInfo[] contentFiles = contentDirInfo.GetFiles();
+            foreach (FileInfo file in contentFiles)
+            {
+                if(file.Name.ToLower().Contains(".pak"))
+                {
+                    WriteColoredLine("This ain't a converter silly!", ConsoleColor.Red);
+                    Exit();
+                }
+            }
+
+            if(!ts2DirExists)
+            {
+                WriteColoredLine("In your selected folder there was no sub-directory found with the name of \"TS2Prototype\"!", ConsoleColor.Red);
+                Exit(3);
+            }
+
+            if (pakName == null || !pakName.ToLower().Contains(".pak")) 
+            {
+                pakName = "TSWpak-output.pak";
+            }
+            
+            if (!pakName.Contains(@":\")) // Check if filepath has the part after the drive letter ":\" in it, if so assume it#s a full file path
+            {
+                if (pakName.Contains("\""))
+                {
+                    pakName.Replace("\"", ""); // Remove quatation marks around the file path as they will be added again a few lines down
+                }
+
+                string pakFilePath = "\"" + Directory.GetCurrentDirectory() + @"\" + pakName + "\"";
+                WriteColoredLine("No full/invalid pak file path (" + pakName + ") specified! Output location: " + pakFilePath, ConsoleColor.DarkGray);
+                pakName = pakFilePath;
+            }
+            #endregion
+
+            string responseFileContent = "\"" + contentPath + "\\*.*\" \"..\\..\\..\\*.*\"";
+            string responseFilePath = Path.GetTempPath() + "TSWpak_responseFile.txt";
+            File.WriteAllText(responseFilePath, responseFileContent);
+
+            string unrealPakArguments = pakName + " -create=" + responseFilePath + " -compress";
+
+            Console.Write("\nUnrealPak.exe output:\n");
+
+            using (Process process = new Process())
+            {
+                ProcessStartInfo processStartInfo = new ProcessStartInfo();
+                processStartInfo.RedirectStandardOutput = true;
+                processStartInfo.UseShellExecute = false;
+                process.StartInfo = processStartInfo;
+
+                process.StartInfo.FileName = unrealPakPath;
+                process.StartInfo.Arguments = unrealPakArguments;
+                process.Start();
+
+                /*if(process.StandardOutput.ReadToEnd().Contains("Error: "))
+                {
+                    ConsoleColor userForeground = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(process.StandardOutput.ReadToEnd());
+                    Console.ForegroundColor = userForeground;
+                }*/
+
+                Console.WriteLine(process.StandardOutput.ReadToEnd());
+                process.WaitForExit();
+            }
+
+            // UnrealPak path is knwon working by now, it can be saved
+            if (unrealPakPath.Contains("\""))
+                unrealPakPath.Replace("\"", "");
+            UpdateAppSetting("UnrealPakPath", unrealPakPath);
+
+            #region Cleanup
+            File.Delete(responseFilePath);
+
+            if (StartedFromGui)
+                Exit(0);
+            #endregion
+        }
+
+        static void WriteColoredLine(string text, ConsoleColor color)
+        {
+            ConsoleColor userForeground = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = userForeground;
+        }
+
+        static void TestDir(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path); // TODO: Filter input. This is a base System.IO method and it can cause BSODs with the wrong inputs lol
+            if (!dir.Exists)
+            {
+                WriteColoredLine(path + " not found!", ConsoleColor.Red);
+                Exit(3);
+            }
+        }
+
+        static void Exit(int exitcode = 0)
+        {
+            if (StartedFromGui)
+            {
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+            Environment.Exit(exitcode);
+        }
+
+        static void UpdateAppSetting(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+        }
+
+        static string ReadSetting(string key)
+        {
+            if(File.Exists(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile))
+            { 
+                try
+                {
+                    var appSettings = ConfigurationManager.AppSettings;
+                    string result = appSettings[key] ?? "Not Found";
+                    return result;
+                }
+                catch (ConfigurationErrorsException)
+                {
+                    WriteColoredLine("Error reading app settings", ConsoleColor.Red);
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ TSWpak/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("TSWpak")]
+[assembly: AssemblyDescription("Tool for easing the packaging process of mods for Train Sim World 2")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Foobian")]
+[assembly: AssemblyProduct("TSWpak")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("03c547e3-4a20-4e8d-a3d7-316ccebbd5a5")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
